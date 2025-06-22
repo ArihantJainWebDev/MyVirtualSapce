cf37aabcaa1771b333bb777340e3e3eef86f44c9
ArihantJainWebDev <arihantjainwebdev@gmail.com>
2025-06-23 01:04:59 +0530

    Goal section created

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
+import GoalsScreen from './screens/GoalsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Goals" component={GoalsScreen} />
         {/* Add more screens later */}
       </Stack.Navigator>
     </NavigationContainer>
